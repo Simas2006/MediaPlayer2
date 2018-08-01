e7c15cb23d3315b1a4d113f8f2f5328d9359ada0
Simas2006 <sjuknelis@gmail.com>
2018-08-01 18:10:30 +0200

    Create title component

--- mp2-mobile-app/App.js
+++ mp2-mobile-app/App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet,Text,View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Title text="Photos" />
+        <Text style={styles.normalText}>ohi</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class Title extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.bigText}>MediaPlayer2</Text>
+        <Text style={styles.titleText}>{this.props.text}</Text>
+        <View style={styles.hr} />
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  bigText: {
+    fontSize: 50,
+    textAlign: "center",
+    paddingTop: "6%",
+    paddingBottom: 0
   },
+  normalText: {
+    fontSize: 25
+  },
+  titleText: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+  hr: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    paddingBottom: "1%"
+  }
 });
