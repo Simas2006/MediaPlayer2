15687b1096877328915fe08fec6c076031e9e5a3
Simas2006 <sjuknelis@gmail.com>
2018-08-08 20:07:02 -0400

    save progress if power out

--- mp2-tv-app/static/interactions.js
+++ mp2-tv-app/static/interactions.js
@@ -12,26 +12,49 @@ function checkForCommand(handlers) {
       if ( err.code == "ENOENT" ) return;
       else throw err;
     }
-    data = data.toString().trim();
+    data = data.toString().trim().split(" ");
     var textToWrite;
-    if ( validateCommand(data) ) {
-      parseCommand(data,handlers,function(toWrite) {
-        fs.writeFile(__dirname + "/../server/outputCmd",toWrite,function(err) {
+    callback(command,function(valid) {
+      if ( valid ) {
+        parseCommand(data,handlers,function(toWrite) {
+          fs.writeFile(__dirname + "/../server/outputCmd",toWrite,function(err) {
+            if ( err ) throw err;
+          });
+        });
+      } else {
+        fs.writeFile(__dirname + "/../server/outputCmd","error",function(err) {
           if ( err ) throw err;
         });
-      });
-    } else {
-      fs.writeFile(__dirname + "/../server/outputCmd","error",function(err) {
-        if ( err ) throw err;
-      });
-    }
+      }
+    });
   });
 }
 
-function validateCommand(command) {
-
+function validateCommand(command,callback) {
+  var paramCount = {
+    "LIST" : 1,
+    "TYPE" : 1,
+    "ADDTQ": 3,
+    "OPENP": 1,
+    "PREVP": 0,
+    "NEXTP": 0,
+    "HOME" : 0,
+    "WOPN" : 1,
+    "PLYPS": 0,
+    "PNSNG": 0,
+    "RWIND": 0,
+    "GETQ" : 0,
+    "UPSQ" : 2,
+    "DWNSQ": 1,
+    "DELSQ": 1,
+    "CLRQ" : 0,
+    "SHFLQ": 0
+  }
+  if ( paramCount[command[0]] != command.length - 1 && paramCount[command[0]] != 3 ) {
+    console.log("NO");
+  }
 }
 
 function parseCommand(command,handlers,callback) {
-  
+
 }
