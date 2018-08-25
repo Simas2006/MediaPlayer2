8a97b608b68d52796abc014a48e05b57aec9272f
Simas2006 <sjuknelis@gmail.com>
2018-08-25 18:27:02 -0400

    Boilerplate for DLP electron app

---
+++ mp2-dlp-app/app/main.js
@@ -0,0 +1,31 @@
+var {app,BrowserWindow,globalShortcut,Menu} = require("electron");
+var fs = require("fs");
+var LOCAL_DIR = process.env.APPDATA || (process.platform == "darwin" ? process.env.HOME + "/Library/Application Support/MediaPlayer2" : "/var/local");
+var window;
+
+function createWindow() {
+  var size = require("electron").screen.getPrimaryDisplay().size;
+  window = new BrowserWindow({
+    width: size.width,
+    height: size.height
+  });
+  window.webContents.openDevTools();
+  window.on("closed",function() {
+    window = null;
+  });
+  window.loadURL(`file://${__dirname}/static/index.html`);
+}
+
+app.on("ready",createWindow);
+
+app.on("window-all-closed",function() {
+  if ( process.platform == "darwin" ) {
+    app.quit();
+  }
+});
+
+app.on("activate",function() {
+  if ( ! window ) {
+    createWindow();
+  }
+});
