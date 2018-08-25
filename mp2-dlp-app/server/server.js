526ecb27bf44e188c5b35a1c61789e13e9a6e2f6
Simas2006 <sjuknelis@gmail.com>
2018-08-25 16:48:21 -0400

    Implement INTDL + DWNLD commands (ie finish server unless theres a bug)

--- mp2-dlp-app/server/server.js
+++ mp2-dlp-app/server/server.js
@@ -1,11 +1,22 @@
 var fs = require("fs");
 var crypto = require("crypto");
+var archiver = require("archiver");
 var express = require("express");
 var app = express();
 var PASSWORD = process.argv[2];
 var PORT = process.argv[3] || 5601;
 var LOCAL_DIR = process.env.APPDATA || (process.platform == "darwin" ? process.env.HOME + "/Library/Application Support/MediaPlayer2" : "/var/local");
 var PHOTO_LOC = LOCAL_DIR + "/LocalData/photos";
+var ivs = {
+  "111": {
+    iv: crypto.randomBytes(16),
+    dir: "nested"
+  },
+  "112": {
+    iv: crypto.randomBytes(16),
+    dir: "nested"
+  }
+};
 
 if ( ! PASSWORD ) throw new Error("No password provided");
 
@@ -50,6 +61,38 @@ app.post("/receive",function(request,response) {
           if ( err ) throw err;
           response.send(files.filter(item => ! item.startsWith(".")).map(item => encodeURIComponent(item)).join(","));
         });
+      } else if ( message[0] == "INTDL" ) {
+        fs.readdir(PHOTO_LOC,function(err,files) {
+          if ( err ) throw err;
+          files = files.filter(item => ! item.startsWith(".")).map(item => encodeURIComponent(item));
+          if ( files.indexOf(message[1]) <= -1 ) {
+            response.send("error");
+            return;
+          }
+          var id = Math.floor(Math.random() * 1e9);
+          ivs[id.toString()] = {
+            iv: crypto.randomBytes(16),
+            dir: message[1]
+          }
+          response.send(cg.encrypt(id + "," + ivs[id].iv.toString("base64"),PASSWORD));
+        });
+      } else if ( message[0] == "DWNLD" ) {
+        if ( ! ivs[message[1]] ) {
+          response.send("error");
+          return;
+        }
+        var cipher = crypto.createCipheriv("aes-256-cbc",new Buffer("/".repeat(32 - PASSWORD.length) + PASSWORD),ivs[message[1]].iv);
+        var archive = archiver("zip",{zlib: 9});
+        archive.on("warning",function(err) {
+          throw err;
+        });
+        archive.on("error",function(err) {
+          throw err;
+        });
+        archive.pipe(cipher);
+        cipher.pipe(response);
+        archive.directory(PHOTO_LOC + "/" + ivs[message[1]].dir,false);
+        archive.finalize();
       }
     }
   });
@@ -57,10 +100,8 @@ app.post("/receive",function(request,response) {
 
 app.get("/blank",function(request,response) {
   response.send("hi");
-})
+});
 
 app.listen(PORT,function() {
   console.log("Listening on port " + PORT);
-  var cg = new Cryptographer();
-  console.log(cg.encrypt("LIST",PASSWORD));
 });
