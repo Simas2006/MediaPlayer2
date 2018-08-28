ebfa8e7ddc8c95e64f599ccd67ba39dd67b18893
Simas2006 <sjuknelis@gmail.com>
2018-08-28 14:49:57 -0400

    Simple photo page implementation based on TV app code

--- mp2-dlp-app/app/static/display.js
+++ mp2-dlp-app/app/static/display.js
@@ -1,8 +1,14 @@
 var {ipcRenderer} = require("electron");
+var EXIF = require("exif-js");
 var LOCAL_DIR = process.env.APPDATA || (process.platform == "darwin" ? process.env.HOME + "/Library/Application Support/MediaPlayer2-dlp" : "/var/local");
 var DATA_LOC = LOCAL_DIR + "/LocalData";
 var hasLoaded = false;
 var folderPath = [];
+var photoIndex = 0;
+
+function replaceAll(oldChar,newChar,string) {
+  return string.split(oldChar).join(newChar);
+}
 
 function drawDownloadPage() {
   if ( ! hasLoaded ) {
@@ -72,6 +78,7 @@ function drawNavigationPage() {
     files = files.filter(item => ! item.startsWith("."));
     for ( var i = 0; i < files.length; i++ ) {
       if ( files[i].charAt(files[i].length - 4) == "." ) {
+        drawPhotoPage();
         openPage("photo");
         return;
       }
@@ -92,6 +99,39 @@ function drawNavigationPage() {
   });
 }
 
+function drawPhotoPage() {
+  function calculateRatio(width,height,maxWidth,maxHeight) {
+    var modifier;
+    if ( width >= maxWidth && height >= maxHeight ) modifier = -0.01;
+    else modifier = 0.01;
+    for ( var i = 1; i > 0; i += modifier ) {
+      if ( modifier == -0.01 && width * i <= maxWidth && height * i <= maxHeight ) return i;
+      else if ( modifier == 0.01 && (width * i > maxWidth || height * i > maxHeight) ) return i - 0.01;
+    }
+    throw new Error("No ratio found");
+  }
+  fs.readdir(DATA_LOC + "/" + folderPath.join("/"),function(err,files) {
+    var photoName = files[photoIndex];
+    var picture = document.getElementById("picture");
+    var img = new Image();
+    img.src = replaceAll("#","%23",replaceAll("?","%3F",DATA_LOC + "/" + folderPath.join("/") + "/" + photoName));
+    img.onload = function() {
+      EXIF.getData(img,function() {
+        var orientation = EXIF.getTag(this,"Orientation");
+        var rotation = [0,0,180,0,0,90,0,270][orientation - 1] || 0;
+        var ratio = calculateRatio(img.width,img.height,window.innerWidth,window.innerHeight);
+        picture.src = img.src;
+        picture.width = img.width * ratio;
+        picture.height = img.height * ratio;
+        picture.onload = function() {
+          picture.style.transform = `rotate(${rotation}deg)`;
+          picture.style.display = "inline";
+        }
+      });
+    }
+  });
+}
+
 function moveBackPage() {
   folderPath.pop();
   if ( folderPath.length > 0 ) {
@@ -104,7 +144,7 @@ function moveBackPage() {
 }
 
 function openPage(toOpen) {
-  var pages = ["download","navigation"];
+  var pages = ["download","navigation","photo"];
   for ( var i = 0; i < pages.length; i++ ) {
     document.getElementById(pages[i] + "Page").className = "hidden";
   }
